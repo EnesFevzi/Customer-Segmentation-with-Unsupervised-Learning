be255523746552fd8e4fe1c2995e2a68670c66c3
Enes Fevzi <118919666+EnesFevzi@users.noreply.github.com>
2023-01-06 15:59:32 +0300

    Update Gözetimsiz Öğrenme ile Müşteri Segmentasyonu.py

--- Gözetimsiz Öğrenme ile Müşteri Segmentasyonu.py
+++ Gözetimsiz Öğrenme ile Müşteri Segmentasyonu.py
@@ -1,60 +1,7 @@
 
-###############################################################
-# Gözetimsiz Öğrenme ile Müşteri Segmentasyonu (Customer Segmentation with Unsupervised Learning)
-###############################################################
 
 ###############################################################
-# İş Problemi (Business Problem)
-###############################################################
-
-# Unsupervised Learning yöntemleriyle (Kmeans, Hierarchical Clustering )  müşteriler kümelere ayrılıp ve davranışları gözlemlenmek istenmektedir.
-
-###############################################################
-# Veri Seti Hikayesi
-###############################################################
-
-# Veri seti son alışverişlerini 2020 - 2021 yıllarında OmniChannel(hem online hem offline) olarak yapan müşterilerin geçmiş alışveriş davranışlarından
-# elde edilen bilgilerden oluşmaktadır.
-
-# 20.000 gözlem, 13 değişken
-
-# master_id: Eşsiz müşteri numarası
-# order_channel : Alışveriş yapılan platforma ait hangi kanalın kullanıldığı (Android, ios, Desktop, Mobile, Offline)
-# last_order_channel : En son alışverişin yapıldığı kanal
-# first_order_date : Müşterinin yaptığı ilk alışveriş tarihi
-# last_order_date : Müşterinin yaptığı son alışveriş tarihi
-# last_order_date_online : Muşterinin online platformda yaptığı son alışveriş tarihi
-# last_order_date_offline : Muşterinin offline platformda yaptığı son alışveriş tarihi
-# order_num_total_ever_online : Müşterinin online platformda yaptığı toplam alışveriş sayısı
-# order_num_total_ever_offline : Müşterinin offline'da yaptığı toplam alışveriş sayısı
-# customer_value_total_ever_offline : Müşterinin offline alışverişlerinde ödediği toplam ücret
-# customer_value_total_ever_online : Müşterinin online alışverişlerinde ödediği toplam ücret
-# interested_in_categories_12 : Müşterinin son 12 ayda alışveriş yaptığı kategorilerin listesi
-# store_type : 3 farklı companyi ifade eder. A company'sinden alışveriş yapan kişi B'dende yaptı ise A,B şeklinde yazılmıştır.
-
-
-###############################################################
-# GÖREVLER
-###############################################################
-
-# GÖREV 1: Veriyi Hazırlama
-           # 1. flo_data_20K.csv.csv verisini okuyunuz.
-           # 2. Müşterileri segmentlerken kullanacağınız değişkenleri seçiniz. Tenure(Müşterinin yaşı), Recency (en son kaç gün önce alışveriş yaptığı) gibi yeni değişkenler oluşturabilirsiniz.
-
-# GÖREV 2: K-Means ile Müşteri Segmentasyonu
-           # 1. Değişkenleri standartlaştırınız.
-           # 2. Optimum küme sayısını belirleyiniz.
-           # 3. Modelinizi oluşturunuz ve müşterilerinizi segmentleyiniz.
-           # 4. Herbir segmenti istatistiksel olarak inceleyeniz.
-
-# GÖREV 3: Hierarchical Clustering ile Müşteri Segmentasyonu
-           # 1. Görev 2'de standırlaştırdığınız dataframe'i kullanarak optimum küme sayısını belirleyiniz.
-           # 2. Modelinizi oluşturunuz ve müşterileriniz segmentleyiniz.
-           # 3. Herbir segmenti istatistiksel olarak inceleyeniz.
-
-
-###############################################################
-# GÖREV 1: Veri setini okutunuz ve müşterileri segmentlerken kullanıcağınız değişkenleri seçiniz.
+#Veri setini okutup ve müşterileri segmentlerken kullanıcağınız değişkenleri seçiyoruz.
 ###############################################################
 
 import pandas as pd
@@ -93,10 +40,10 @@ model_df = df[["order_num_total_ever_online","order_num_total_ever_offline","cus
 model_df.head()
 
 ###############################################################
-# GÖREV 2: K-Means ile Müşteri Segmentasyonu
+#K-Means ile Müşteri Segmentasyonu
 ###############################################################
 
-# 1. Değişkenleri standartlaştırınız.
+# 1. Değişkenleri standartlaştırması
 #SKEWNESS
 def check_skew(df_skew, column):
     skew = stats.skew(df_skew[column])
@@ -139,14 +86,14 @@ model_df=pd.DataFrame(model_scaling,columns=model_df.columns)
 model_df.head()
 
 
-# 2. Optimum küme sayısını belirleyiniz.
+# 2. Optimum küme sayısını belirleme
 kmeans = KMeans()
 elbow = KElbowVisualizer(kmeans, k=(2, 20))
 elbow.fit(model_df)
 elbow.show()
 
 
-# 3. Modelinizi oluşturunuz ve müşterilerinizi segmentleyiniz.
+# 3. Modelinizi oluşturunuz ve müşterilerinizi segmentleme
 k_means = KMeans(n_clusters = 7, random_state= 42).fit(model_df)
 segments=k_means.labels_
 segments
@@ -166,10 +113,10 @@ final_df.groupby("segment").agg({"order_num_total_ever_online":["mean","min","ma
 
 
 ###############################################################
-# GÖREV 3: Hierarchical Clustering ile Müşteri Segmentasyonu
+#Hierarchical Clustering ile Müşteri Segmentasyonu
 ###############################################################
 
-# 1. Görev 2'de standarlaştırdığınız dataframe'i kullanarak optimum küme sayısını belirleyiniz.
+#standarlaştırdığınız dataframe'i kullanarak optimum küme sayısını belirlenmesi
 hc_complete = linkage(model_df, 'complete')
 
 plt.figure(figsize=(7, 5))
@@ -183,7 +130,7 @@ plt.axhline(y=1.2, color='r', linestyle='--')
 plt.show()
 
 
-# 2. Modelinizi oluşturunuz ve müşterileriniz segmentleyiniz.
+# Modelinizi oluşturulması ve müşterilerin segmentlenmesi.
 hc = AgglomerativeClustering(n_clusters=5)
 segments = hc.fit_predict(model_df)
 
@@ -191,7 +138,7 @@ final_df = df[["master_id","order_num_total_ever_online","order_num_total_ever_o
 final_df["segment"] = segments
 final_df.head()
 
-# 3. Herbir segmenti istatistiksel olarak inceleyeniz.
+# Herbir segmenti istatistiksel olarak incelenmesi
 final_df.groupby("segment").agg({"order_num_total_ever_online":["mean","min","max"],
                                   "order_num_total_ever_offline":["mean","min","max"],
                                   "customer_value_total_ever_offline":["mean","min","max"],
